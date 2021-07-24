f9358f73b10e7a423e52f5f57ce33e94a5b8906a
Felipe Pinto Da Silva <64941387+FelipePDS@users.noreply.github.com>
2021-07-23 23:33:27 -0300

    bakery website - razor page

---
+++ Bakery/Data/ModelBuilderExtensions.cs
@@ -0,0 +1,60 @@
+using Bakery.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bakery.Data
+{
+    public static class ModelBuilderExtensions
+    {
+        public static ModelBuilder Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Name = "Carrot Cake",
+                    Description = "A scrumptious mini-carrot cake encrusted with sliced almonds",
+                    Price = 8.99m,
+                    ImageName = "carrot_cake.jpg"
+                },
+
+                new Product
+                {
+                    Id = 2,
+                    Name = "Lemon Tart",
+                    Description = "A delicious lemon tart with fresh meringue cooked to perfection",
+                    Price = 9.99m,
+                    ImageName = "lemon_start.jpg"
+                },
+
+                new Product
+                {
+                    Id = 3,
+                    Name = "Cupcakes",
+                    Description = "Delectable vanilla and chocolate cupcakes",
+                    Price = 5.99m,
+                    ImageName = "cupcakes.jpg"
+                },
+
+                new Product
+                {
+                    Id = 4,
+                    Name = "Bread",
+                    Description = "Fresh baked Frech-style bread",
+                    Price = 1.49m,
+                    ImageName = "pear_tart.jpg"
+                },
+
+                new Product
+                {
+                    Id = 6,
+                    Name = "Chocolate Cake",
+                    Description = "Rich Chocolate frosting cover this chocolate lover's dream",
+                    Price = 8.99m,
+                    ImageName = "chocolate_cake.jpg"
+                }
+            );
+
+            return modelBuilder;
+        }
+    }
+}
