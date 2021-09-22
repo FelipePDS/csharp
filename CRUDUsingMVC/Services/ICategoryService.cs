cdaa936431c4034ef55340d6fc934783797a499c
Felipe Pinto Da Silva <64941387+FelipePDS@users.noreply.github.com>
2021-09-22 16:55:17 -0300

    add new generic methods to repository

---
+++ CRUDUsingMVC/Services/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using CRUDUsingMVC.Models;
+
+namespace CRUDUsingMVC.Services
+{
+    public interface ICategoryService
+    {
+        List<Category> GetAll();
+
+        Category GetById(int id);
+
+        void Create(Category category);
+
+        void UpdateById(Category category, int id);
+
+        void DeleteById(int id);
+    }
+}
