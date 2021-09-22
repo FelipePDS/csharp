e668ee0a8c60f9733c7159a3fb1d1465e9e41ea0
Felipe Pinto Da Silva <64941387+FelipePDS@users.noreply.github.com>
2021-09-22 11:18:34 -0300

    WebApi consumption and creation

---
+++ WebApiConsumptionAndCreation/Consumir_WebApi2_Produtos/Consumir_WebApi2_Produtos/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Consumir_WebApi2_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunAsync().Wait();
+
+            Console.ReadKey();
+        }
+
+        static async Task RunAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new System.Uri("http://localhost:53557/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                MostrarProdutos(client);
+
+                Produto produtoCha = new Produto() { Nome = "Chá", Categoria = "Bebidas", Preco = 1.50M };
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("api/produtos", produtoCha);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    MostrarProdutos(client);
+
+                    Console.WriteLine("Produto cha verde incluído. Tecle algo para continuar");
+                    Console.ReadKey();
+
+                    Uri urlProdutoCha = response.Headers.Location;
+                    produtoCha.Preco = 2.50M;
+                    response = await client.PutAsJsonAsync(urlProdutoCha, produtoCha);
+
+                    Console.WriteLine("Produto preço do atualizado. Tecle algo para excluir o produto");
+                    Console.ReadKey();
+
+                    MostrarProdutos(client);
+
+                    response = await client.DeleteAsync(urlProdutoCha);
+                    Console.WriteLine("Produto deletado");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        static async void MostrarProdutos(HttpClient client)
+        {
+            HttpResponseMessage response = await client.GetAsync("/api/produtos");
+
+            if (response.IsSuccessStatusCode)
+            {
+                IEnumerable<Produto> listaProduto = await response.Content.ReadAsAsync<IEnumerable<Produto>>();
+
+                foreach (var produto in listaProduto)
+                {
+                    Console.WriteLine("{0}\tR${1}\t{2}\n", produto.Nome, produto.Preco, produto.Categoria);
+                }
+
+                Console.WriteLine("Produtos acessados e exibidos. Tecle algo para continuar!");
+                Console.ReadKey();
+            }
+        }
+    }
+}
