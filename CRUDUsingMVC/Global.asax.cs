e4ce4184f63e610962f831104b4b3f97b7c9006b
Felipe Pinto Da Silva <64941387+FelipePDS@users.noreply.github.com>
2021-09-22 16:54:19 -0300

    add new generic methods to repository

---
+++ CRUDUsingMVC/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CRUDUsingMVC.Services;
+using CRUDUsingMVC.Data;
+using Unity;
+using Unity.Injection;
+
+namespace CRUDUsingMVC
+{
+    public class MvcApplication : HttpApplication
+    {
+        static readonly IUnityContainer container = new UnityContainer();
+
+        protected void Application_Start()
+        {
+            //container.RegisterType<ICategoryRepository, CategoryRepository>(new InjectionConstructor());
+            //container.RegisterType<IProductRepository, ProductRepository>(new InjectionConstructor());
+
+            container.RegisterType<ICategoryService, CategoryService>(new InjectionConstructor());
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
